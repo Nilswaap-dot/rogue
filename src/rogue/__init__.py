c5969cd110f9a75748f7af2135e3644f7cc12946
maltekliemann <mail@maltekliemann.com>
2021-06-21 11:58:58 +0200

    Add missing license stubs

--- src/rogue/__init__.py
+++ src/rogue/__init__.py
@@ -1,1 +1,5 @@
+# SPDX-FileCopyrightText: 2021 Forschungs- und Entwicklungszentrum Fachhochschule Kiel GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from rogue.rogue import RogueException, Server
